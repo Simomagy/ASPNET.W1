de0d8c16938ad25c62956de6e2d5cf9b96d39501
StealthBlack98 <gennaro.boccardi1@gmail.com>
2025-01-16 11:47:28 +0100

    Controllers Finiti

---
+++ Ospedale/Controllers/MediciController.cs
@@ -0,0 +1,59 @@
+﻿using Ospedale.Models;
+using Microsoft.AspNetCore.Mvc;
+using MSSTU.DB.Utility;
+
+namespace Ospedale.Controllers
+{
+    public class MediciController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Lista()
+        {
+            return View(DAOMedici.GetInstace().GetRecords());
+        }
+
+        public IActionResult Dettagli(int id)
+        {
+            return View(DaoMedici.GetInstance().FindRecord(id));
+        }
+        public IActionResult? Elimina(int id)
+        {
+            if (DAOMedici.GetInstance().DeleteRecord(id))
+                return RedirectToAction("Lista");
+            return Content($"Errore nell'eliminazione del medico con id {id}");
+        }
+        public IActionResult FormInserisci()
+        {
+            return View("FormInserisci");
+        }
+        public IActionResult Inserisci(Dictionary<string, string> parameters)
+        {
+            Entity e = new Medico();
+            e.TypeSort(parameters);
+
+            if (DAOMedici.GetInstance().CreateRecord(e))
+                return RedirectToAction("Lista");
+            return Content($"Errore nella creazione del medico {e.ToString()}");
+        }
+        public IActionResult Modifica(Dictionary<string, string> parameters)
+        {
+            Entity e = new Medico();
+            e.TypeSort(parameters);
+
+            if (DAOMedici.GetInstance().UpdateRecord(e))
+                return RedirectToAction("Lista");
+            return Content($"Errore nella modifica del medico {e.ToString()}");
+        }
+        public IActionResult FormModifica()
+        {
+            var medico = DAOMedici.GetInstance().FindRecord(id);
+            if (medico == null)
+                return Content($"Errore nel reperimento del medico con id {id}");
+            return View((Medico)medico);
+        }
+    }
+}
---
+++ Ospedale/Controllers/OspedaliController.cs
@@ -0,0 +1,57 @@
+﻿using Ospedale.Models;
+using Microsoft.AspNetCore.Mvc;
+using MSSTU.DB.Utility;
+
+namespace Ospedale.Controllers
+{
+    public class OspedaliController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Lista()
+        {
+            return View(DAOOspedali.GetInstance.GetRecords());
+        }
+        public IActionResult Dettagli(int id)
+        {
+            return View(DAOOspedali.GetInstance.FindRecords(id));
+        }
+        public IActionResult? Elimina(int id)
+        {
+            if (DAOOspedali.GetInstance().DeleteRecord(id))
+                return RedirectToAction("Lista");
+            return Content($"Errore nell'eliminazione dell' ospedale con id {id}");
+        }
+        public IActionResult FormInserisci()
+        {
+            return View("FormInserisci");
+        }
+        public IActionResult Inserisci(Dictionary<string, string> parameters)
+        {
+            Entity e = new Ospedale();
+            e.TypeSort(parameters);
+
+            if (DAOOspedali.GetInstance().CreateRecord(e))
+                return RedirectToAction("Lista");
+            return Content($"Errore nella creazione dell'ospedale {e.ToString()}");
+        }
+        public IActionResult Modifica(Dictionary<string, string> parameters)
+        {
+            Entity e = new Ospedali();
+            e.TypeSort(parameters);
+            if (DAOOspedali.GetInstance().UpdateRecord(e))
+                return RedirectToAction("Lista");
+            return Content($"Errore nella modifica dell'ospedale {e.ToString()}");
+        }
+        public IActionResult FormModifica(int id)
+        {
+            var ospedale = DAOOspedali.GetInstance().FindRecord(id);
+            if (ospedale == null)
+                return Content($"Errore nel reperimento dell'ospedale con id {id}");
+            return View((Ospedale)ospedale);
+        }
+    }
+}
